447bde5c7e5a3d6fc563639bbfbe2ab6419e8fae
widewu <979610327@qq.com>
2022-10-25 23:04:21 +0800

    feat: 初始化总结的模版

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'widewu的总结上分之路',
+  description: 'Just playing around.',
+  themeConfig: {
+    siteTitle: 'My Custom Title',
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ]
+  }
+}
