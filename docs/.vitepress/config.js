8158a9f626ad1949ff064db0d246e6c1902b47c1
widewu <widewu@tencent.com>
2022-10-26 13:51:22 +0800

    feat:  增加菜单和构建逻辑

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,56 @@
 module.exports = {
   title: 'widewu的总结上分之路',
   description: 'Just playing around.',
+  base: '/Day-Day-Up',
   themeConfig: {
-    siteTitle: 'My Custom Title',
+    siteTitle: '上分，上分',
     sidebar: [
       {
-        text: 'Guide',
+        text: 'JS基础',
         items: [
           { text: 'Introduction', link: '/introduction' },
           { text: 'Getting Started', link: '/getting-started' },
         ]
+      },
+      {
+        text: 'ES6-ES12',
+        items: []
+      },
+      {
+        text: 'Vue',
+        items: []
+      },
+      {
+        text: 'React',
+        items: [],
+      },
+      {
+        text: 'Webpack',
+        items: []
+      },
+      {
+        text: 'Vite',
+        items: []
+      },
+      {
+        text: '微前端',
+        items: []
+      },
+      {
+        text: 'Typescript',
+        items: []
+      },
+      {
+        text: 'NodeJs',
+        items: []
+      },
+      {
+        text: 'Go',
+        items: []
+      },
+      {
+        text: '杂谈',
+        items: [],
       }
     ]
   }
